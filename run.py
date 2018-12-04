7bc9f9a15fb6af1399f94bd0e2642d2d59cbb3dd
Zhenduo Wang <30311623+zhenduow@users.noreply.github.com>
2018-12-03 17:08:08 -0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import networkx as nx
 from numpy import random
+import quantumrandom
 import copy
 import timeit
 
@@ -9,38 +10,38 @@ def greedy(G, desired_set_size = 10, inner_sim_epoch = 10):
     number_of_nodes = nx.number_of_nodes(G)
     number_of_edges = nx.number_of_edges(G)
 
-    chosen = [0] * number_of_nodes
+    chosen = []
 
     for i in range(desired_set_size):
         max_score = 0
         max_score_edge = 0
-        for j in range(number_of_nodes):
+        for j in G.nodes():
             #generating score for each node j
-            if chosen[j] == 0:
+            if j not in chosen:
                 score = 0
                 new_chosen = copy.deepcopy(chosen)
-                new_chosen[j] = 1
+                new_chosen.append(j)
                 for iter in range(inner_sim_epoch):
-                    score += sum(IC(G, new_chosen))
+                    score += len(IC(G, new_chosen))
 
                 if score > max_score:
                     max_score = score
-                    max_score_edge = j
+                    max_score_node = j
 
-        chosen[max_score_edge] = 1
-        print("Iter = ",i , ", vertex ", max_score_edge, " is chosen with score ", max_score/inner_sim_epoch)
+        chosen.append(max_score_node)
+        print(chosen)
 
-    return sum(IC(G, chosen))
+        print("Iter = ", j , ", vertex ", max_score_edge, " is chosen with score ", max_score/inner_sim_epoch)
 
-def high_degree(G, activating_probability, desired_set_size = 10, inner_sim_epoch = 10):
+    return len(IC(G, chosen))
+
+def high_degree(G, desired_set_size = 10, inner_sim_epoch = 10):
     number_of_nodes = nx.number_of_nodes(G)
     degree_sequence = sorted(G.degree, key=lambda x: x[1], reverse=True)
     chosen_set = [name for (name, degree) in degree_sequence[:desired_set_size]]
-    chosen_list = [0] * number_of_nodes
-    for i in range(number_of_nodes):
-        chosen_list[i] = 1 if str(i) in chosen_set else 0
-    
-    return sum(IC(G, chosen_list, activating_probability))
+
+    return len(IC(G, chosen_set))
+
         
 def hdGreedy(G, k):
     #degree_sequence = sorted([d for n, d in G.degree()])
@@ -59,7 +60,7 @@ def hdGreedy(G, k):
     return len(g)
 
 
-def IC(G, initial_set,activating_probability):
+def IC(G, initial_set):
     number_of_nodes = nx.number_of_nodes(G)
     new_node_activated = True
     activated_nodes = copy.deepcopy(initial_set)
@@ -67,25 +68,24 @@ def IC(G, initial_set,activating_probability):
     while new_node_activated:
         # when some node is activated
         new_node_activated = False
-        nodes_activation = [0] * number_of_nodes
+        nodes_activation = []
         for i in newly_activated_nodes:
-            randn = random.rand(number_of_nodes)
             for j in G.neighbors(str(i)):
-                if randn[int(j)] < activating_probability[int(i)][int(j)] and activated_nodes[int(j)] == 0:
+                if random.uniform(0,1) < G.edges[u, v]['weight'] and j not in activated_nodes:
                     # if the random number is greater than the activation probability
-                    nodes_activation[int(j)] = 1
-                    activated_nodes[int(j)] = 1
+                    nodes_activation.append(j)
+                    activated_nodes.append(j)
                     new_node_activated = True
-        newly_activated_nodes = []
-        for k in range(number_of_nodes):
-            if nodes_activation[k] == 1:
-                newly_activated_nodes.append(k)
+        newly_activated_nodes = copy.deepcopy(nodes_activation)
 
     return activated_nodes
+
 
 if __name__ == "__main__":
     G = nx.read_edgelist("./1005edges")
     #G = nx.read_edgelist("./toy")
+    for u,v,e in G.edges(data = True):
+        e['weight'] = random.uniform(0,1)/10
 
     start = timeit.timeit()
     print("High-degree algorithm final spread {}".format(high_degree(G)))
